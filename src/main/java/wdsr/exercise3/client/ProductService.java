457aee8f6a4169ab39527b39211be02d1d1b48ac
radekkania <radekkania7@wp.pl>
2017-04-11 16:46:29 +0200

    part implementation of rest client

--- src/main/java/wdsr/exercise3/client/ProductService.java
+++ src/main/java/wdsr/exercise3/client/ProductService.java
@@ -1,11 +1,19 @@
 package wdsr.exercise3.client;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import wdsr.exercise3.model.Product;
 import wdsr.exercise3.model.ProductType;
@@ -21,17 +29,43 @@ public class ProductService extends RestClientBase {
 	 * @return A list of found products - possibly empty, never null.
 	 */
 	public List<Product> retrieveProducts(Set<ProductType> types) {
-		// TODO
-		return null;
-	}
+		Invocation.Builder request = baseTarget.path("products")
+				.queryParam("type", types)
+				.request(MediaType.APPLICATION_JSON);
+		Response response = request.get();
+				
+		if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+			return request.get(new GenericType<List<Product>>() {});
+		} 
+		return new ArrayList<Product>();
+		/*
+		WebTarget target = baseTarget.path("products");
+		for (ProductType type : types) {
+			target = target.queryParam("type", type);
+		}
+		Response response = target.request().get();
+		
+		if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+			return target.request().get(new GenericType<List<Product>>() {});
+		}
+		
+		if (response.getStatus() == Response.Status.NOT_FOUND.getStatusCode()) {
+			return new ArrayList<Product>();
+		}
+		
+		return new ArrayList<Product>(); */
+	} 
 	
 	/**
 	 * Looks up all products known to the server.
 	 * @return A list of all products - possibly empty, never null.
 	 */
 	public List<Product> retrieveAllProducts() {
-		// TODO
-		return null;
+		List<Product> products = baseTarget.path("products")
+				.request(MediaType.APPLICATION_JSON)
+				.get(new GenericType<List<Product>>() {});
+		
+		return products;
 	}
 	
 	/**
@@ -41,8 +75,15 @@ public class ProductService extends RestClientBase {
 	 * @throws NotFoundException if no product found for the given ID.
 	 */
 	public Product retrieveProduct(int id) {
-		// TODO
-		return null;
+		WebTarget target = baseTarget.path("products").path(String.valueOf(id));
+		Invocation.Builder request = target.request(MediaType.APPLICATION_JSON);
+		Response response = request.get();
+		
+		if (response.getStatus() == Response.Status.NOT_FOUND.getStatusCode())
+		{
+			throw new NotFoundException();
+		}
+		return response.readEntity(Product.class);
 	}	
 	
 	/**
@@ -52,8 +93,15 @@ public class ProductService extends RestClientBase {
 	 * @throws WebApplicationException if request to the server failed
 	 */
 	public int storeNewProduct(Product product) {
-		// TODO
-		return 0;
+		Response response = baseTarget.path("products")
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.entity(product, MediaType.APPLICATION_JSON),Response.class);
+		
+		if (response.getStatus() == Status.CREATED.getStatusCode()) {
+			return response.readEntity(Product.class).getId();
+		} 
+		
+		throw new WebApplicationException();
 	}
 	
 	/**
@@ -62,7 +110,13 @@ public class ProductService extends RestClientBase {
 	 * @throws NotFoundException if no product found for the given ID.
 	 */
 	public void updateProduct(Product product) {
-		// TODO
+		WebTarget target = baseTarget.path("/products").path(String.valueOf(product.getId()));
+		Invocation.Builder request = target.request();
+		Response response = request.put(Entity.entity(product, MediaType.APPLICATION_JSON));
+		
+		if (response.getStatus() == Response.Status.NOT_FOUND.getStatusCode()) {
+			throw new NotFoundException();
+		}
 	}
 
 	
@@ -72,6 +126,12 @@ public class ProductService extends RestClientBase {
 	 * @throws NotFoundException if no product found for the given ID.
 	 */
 	public void deleteProduct(Product product) {
-		// TODO
+		WebTarget target = baseTarget.path("/products").path(String.valueOf(product.getId()));
+		Invocation.Builder request = target.request();
+		Response response = request.delete();
+		
+		if (response.getStatus() == Response.Status.NOT_FOUND.getStatusCode()) {
+			throw new NotFoundException();
+		}
 	}
 }
